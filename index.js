2649c39d6a0f7c3504d15861c415a04d4437d254
Hannah Shine <hshine@visitationacademy.org>
2018-06-22 18:44:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+// let countdown = 10;
+//     while (countdown>0){
+//       console.log(--countdown)
+//     }
+    
+
+function tailsNeverFails(){
+  let tails=0;
+  
+  while (Math.random()>=0.5){
+    tails++
+  }
+  console.log( `You got ${tails} tails in a row!`);
+}
+
+tailsNeverFails()
